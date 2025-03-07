8d9490fc168e15b29c9dfe734bed7bc52594982b
ICT Office <icto@ICTs-iMac-10.local>
2025-03-07 09:42:09 +0800

    started guest layout

--- resources/js/Pages/Rental/Create.vue
+++ resources/js/Pages/Rental/Create.vue
@@ -314,18 +314,32 @@ export default {
                     <p class="mb-2">Payment Methods</p>
                     <div class="p-3 md:p-5">
                         <ul class="space-y-2 mb-1">
-                            <li v-for="(item,index) in payments" :key="index">
-                                <input v-model="form.payment_id" type="radio" id="job-1" name="job" :value="item.id" class="hidden peer" required />
-                                <label for="job-1" class="inline-flex items-center justify-between w-full p-3 text-gray-900 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-500 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 dark:peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-900 hover:bg-gray-100 dark:text-white dark:bg-gray-600 dark:hover:bg-gray-500">
+                            <li v-for="(item, index) in payments" :key="index">
+                                <input
+                                    v-model="form.payment_id"
+                                    type="radio"
+                                    :id="item.id"
+                                    name="job"
+                                    :value="item.id"
+                                    class="hidden peer"
+                                    required
+                                />
+                                <label
+                                    :for="item.id"
+                                    class="inline-flex items-center justify-between w-full p-3 text-gray-900 bg-white border border-gray-200 rounded-lg cursor-pointer dark:hover:text-gray-300 dark:border-gray-500 dark:peer-checked:text-blue-500 peer-checked:border-blue-600 dark:peer-checked:border-blue-600 peer-checked:text-blue-600 hover:text-gray-900 hover:bg-gray-100 dark:text-white dark:bg-gray-600 dark:hover:bg-gray-500"
+                                >
                                     <div class="block">
                                         <div class="w-full text-lg font-semibold">{{ item.name }}</div>
                                         <div class="w-full text-gray-500 dark:text-gray-400">{{ item.account_number }}</div>
                                     </div>
 
-                                    <svg class="w-4 h-4 ms-3 rtl:rotate-180 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10"><path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/></svg>
+                                    <svg class="w-4 h-4 ms-3 rtl:rotate-180 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 14 10">
+                                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 5h12m0 0L9 1m4 4L9 9"/>
+                                    </svg>
                                 </label>
                             </li>
                         </ul>
+
                     </div>
 
                 </div>
